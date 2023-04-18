89552daae4e18a7fcad69092fbd0ff50fb39d8c2
CJ <cj@CJs-MacBook-Pro.local>
2023-04-18 04:33:08 +0100

    initial commit

---
+++ hello/hello.c
@@ -0,0 +1,17 @@
+/** 
+* Author: Auspicious Raven
+* Date: 2023/04/14
+*
+* A simple hello world programme in C for Github repository
+*
+*/
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello Chris!\n");
+    printf("Let's add this file to Github!\n");
+
+    return 0;
+}
